2081cd85a7df127353802f8877cc06cd7de18e3a
leoaltemari <leo.altemari@gmail.com>
2020-09-04 10:12:50 -0300

    create dev

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./styles/App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>Hello dev</h1>
+		</div>
+	);
 }
 
 export default App;
